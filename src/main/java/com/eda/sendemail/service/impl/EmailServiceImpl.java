2ee9f025242165f1e979ceb227604ac066612a8a
Gobina Daniel <86768278+GobinaMbale@users.noreply.github.com>
2023-11-14 23:42:58 -0500

    optimisation des methodes de la classe EmailServiceImpl

--- src/main/java/com/eda/sendemail/service/impl/EmailServiceImpl.java
+++ src/main/java/com/eda/sendemail/service/impl/EmailServiceImpl.java
@@ -5,6 +5,7 @@ import jakarta.activation.DataHandler;
 import jakarta.activation.DataSource;
 import jakarta.activation.FileDataSource;
 import jakarta.mail.BodyPart;
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.internet.MimeMultipart;
@@ -29,15 +30,25 @@ import static com.eda.sendemail.utils.EmailUtils.getEmailMessage;
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
     private static final String NEW_USER_ACCOUNT_VERIFICATION = "New User Account Verification";
+    private static final String ACCOUNT_VERIFICATION = "Account Verification";
     private static final String UTF_8_ENCODING = "UTF-8";
-    public static final String EMAILTEMPLATE = "emailtemplate";
-    public static final String TEXT_HTML_ENCODING = "text/html";
+    private static final String EMAILTEMPLATE = "emailtemplate";
+    private static final String TEXT_HTML_ENCODING = "text/html";
+    private static final String USER_HOME = "user.home";
     private final TemplateEngine templateEngine;
     private final JavaMailSender emailSender;
     @Value("${spring.mail.verify.host}")
     private String host;
     @Value("${spring.mail.username}")
     private String fromEmail;
+    @Value("${variable.img_1}")
+    private String img1;
+    @Value("${variable.img_2}")
+    private String img2;
+    @Value("${variable.img_logo}")
+    private String imgLogo;
+    @Value("${variable.document_file}")
+    private String documentFile;
 
     @Override
     @Async
@@ -60,19 +71,9 @@ public class EmailServiceImpl implements EmailService {
     public void sendMimeMessageWithAttachments(String name, String to, String token) {
         try {
             MimeMessage message = getMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, UTF_8_ENCODING);
-            helper.setPriority(1);
-            helper.setSubject(NEW_USER_ACCOUNT_VERIFICATION);
-            helper.setFrom(fromEmail);
-            helper.setTo(to);
-            helper.setText(getEmailMessage(name, host, token));
+            MimeMessageHelper helper = createMimeMessageHelper(message, to, ACCOUNT_VERIFICATION, fromEmail, getEmailMessage(name, host, token), false, true);
             // TODO Add attachments
-            FileSystemResource woman = new FileSystemResource(new File(System.getProperty("user.home")  + "/Downloads/images/woman.png"));
-            FileSystemResource icon = new FileSystemResource(new File(System.getProperty("user.home")  + "/Downloads/images/icon.png"));
-            FileSystemResource data = new FileSystemResource(new File(System.getProperty("user.home")  + "/Downloads/images/data.docx"));
-            helper.addAttachment(woman.getFilename(), woman);
-            helper.addAttachment(icon.getFilename(), icon);
-            helper.addAttachment(data.getFilename(), data);
+            addAttachments(helper);
             emailSender.send(message);
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
@@ -85,19 +86,9 @@ public class EmailServiceImpl implements EmailService {
     public void sendMimeMessageWithEmbeddedImages(String name, String to, String token) {
         try {
             MimeMessage message = getMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, UTF_8_ENCODING);
-            helper.setPriority(1);
-            helper.setSubject(NEW_USER_ACCOUNT_VERIFICATION);
-            helper.setFrom(fromEmail);
-            helper.setTo(to);
-            helper.setText(getEmailMessage(name, host, token));
+            MimeMessageHelper helper = createMimeMessageHelper(message, to, NEW_USER_ACCOUNT_VERIFICATION, fromEmail, getEmailMessage(name, host, token), false, true);
             // TODO Add attachments
-            FileSystemResource woman = new FileSystemResource(new File(System.getProperty("user.home")  + "/Downloads/images/woman.png"));
-            FileSystemResource icon = new FileSystemResource(new File(System.getProperty("user.home")  + "/Downloads/images/icon.png"));
-            FileSystemResource data = new FileSystemResource(new File(System.getProperty("user.home")  + "/Downloads/images/data.docx"));
-            helper.addInline(getContentId(woman.getFilename()), woman);
-            helper.addInline(getContentId(icon.getFilename()), icon);
-            helper.addInline(getContentId(data.getFilename()), data);
+            addEmbeddedImages(helper);
             emailSender.send(message);
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
@@ -110,17 +101,10 @@ public class EmailServiceImpl implements EmailService {
     public void sendHtmlEmail(String name, String to, String token) {
         try {
             Context context = new Context();
-            /*context.setVariable("name", name);
-            context.setVariable("url", getVerificationUrl(host, token));*/
             context.setVariables(Map.of("name", name, "url", getVerificationUrl(host, token)));
             String text = templateEngine.process(EMAILTEMPLATE, context);
             MimeMessage message = getMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, UTF_8_ENCODING);
-            helper.setPriority(1);
-            helper.setSubject(NEW_USER_ACCOUNT_VERIFICATION);
-            helper.setFrom(fromEmail);
-            helper.setTo(to);
-            helper.setText(text, true);
+            createMimeMessageHelper(message, to, NEW_USER_ACCOUNT_VERIFICATION, fromEmail, text, true, true);
             emailSender.send(message);
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
@@ -134,30 +118,16 @@ public class EmailServiceImpl implements EmailService {
         try {
 
             MimeMessage message = getMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, UTF_8_ENCODING);
-            helper.setPriority(1);
-            helper.setSubject(NEW_USER_ACCOUNT_VERIFICATION);
-            helper.setFrom(fromEmail);
-            helper.setTo(to);
-            //helper.setText(text, true);
+            createMimeMessageHelper(message, to, NEW_USER_ACCOUNT_VERIFICATION, fromEmail, "", false, false);
 
             Context context = new Context();
-            /*context.setVariable("name", name);
-            context.setVariable("url", getVerificationUrl(host, token));*/
             context.setVariables(Map.of("name", name, "url", getVerificationUrl(host, token)));
             String text = templateEngine.process(EMAILTEMPLATE, context);
             // TODO HTML email body
-            MimeMultipart mimeMultipart = new MimeMultipart("related");
-            BodyPart messageBodyPart = new MimeBodyPart();
-            mimeMultipart.addBodyPart(messageBodyPart);
-            messageBodyPart.setContent(text, TEXT_HTML_ENCODING);
+            MimeMultipart mimeMultipart = addMimeMultipart(text);
 
             // TODO Add images to the email body
-            BodyPart imageBodyPart = new MimeBodyPart();
-            DataSource dataSource = new FileDataSource(System.getProperty("user.home")  + "/Downloads/images/logo.png");
-            imageBodyPart.setDataHandler(new DataHandler(dataSource));
-            imageBodyPart.setHeader("Content-ID", "image");
-            mimeMultipart.addBodyPart(imageBodyPart);
+            addImageBodyPart(mimeMultipart);
 
             message.setContent(mimeMultipart);
 
@@ -174,5 +144,54 @@ public class EmailServiceImpl implements EmailService {
 
     private String getContentId(String filename) {
         return "<" + filename + ">";
+    }
+
+    private void addImageBodyPart(MimeMultipart mimeMultipart) throws MessagingException {
+        BodyPart imageBodyPart = new MimeBodyPart();
+        DataSource dataSource = new FileDataSource(System.getProperty(USER_HOME)  + imgLogo);
+        imageBodyPart.setDataHandler(new DataHandler(dataSource));
+        imageBodyPart.setHeader("Content-ID", "image");
+        mimeMultipart.addBodyPart(imageBodyPart);
+    }
+
+    private MimeMultipart addMimeMultipart(String text) throws MessagingException {
+        MimeMultipart mimeMultipart = new MimeMultipart("related");
+        BodyPart messageBodyPart = new MimeBodyPart();
+        mimeMultipart.addBodyPart(messageBodyPart);
+        messageBodyPart.setContent(text, TEXT_HTML_ENCODING);
+        return  mimeMultipart;
+    }
+
+    private void addAttachments(MimeMessageHelper helper) throws MessagingException {
+        FileSystemResource woman = new FileSystemResource(new File(System.getProperty(USER_HOME) + img1));
+        FileSystemResource icon = new FileSystemResource(new File(System.getProperty(USER_HOME) + img2));
+        FileSystemResource data = new FileSystemResource(new File(System.getProperty(USER_HOME) + documentFile));
+
+        helper.addAttachment(woman.getFilename(), woman);
+        helper.addAttachment(icon.getFilename(), icon);
+        helper.addAttachment(data.getFilename(), data);
+    }
+
+    private void addEmbeddedImages(MimeMessageHelper helper) throws MessagingException {
+        FileSystemResource woman = new FileSystemResource(new File(System.getProperty(USER_HOME) + img1));
+        FileSystemResource icon = new FileSystemResource(new File(System.getProperty(USER_HOME) + img2));
+        FileSystemResource data = new FileSystemResource(new File(System.getProperty(USER_HOME) + documentFile));
+
+        helper.addInline(getContentId(woman.getFilename()), woman);
+        helper.addInline(getContentId(icon.getFilename()), icon);
+        helper.addInline(getContentId(data.getFilename()), data);
+    }
+
+    private MimeMessageHelper createMimeMessageHelper(MimeMessage message, String to, String subject, String from, String text, Boolean value, Boolean textIsUse) throws MessagingException {
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, UTF_8_ENCODING);
+        helper.setPriority(1);
+        helper.setSubject(subject);
+        helper.setFrom(from);
+        helper.setTo(to);
+        if(Boolean.TRUE.equals(textIsUse)) {
+            helper.setText(text, value);
+        }
+
+        return helper;
     }
 }
